f31105aad37f7f1ae928aa502b7c8aaba7020d0c
Zhirui Wang <576572949@qq.com>
2018-07-09 17:25:52 -0400

    Finish Home Page

---
+++ client/pages/home/home.js
@@ -0,0 +1,97 @@
+// pages/home/home.js
+const qcloud = require('../../vendor/wafer2-client-sdk/index.js')
+const config = require('../../config.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movieList: [], 
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  getmovieList() {
+    wx.showLoading({
+      title: '电影数据加载中',
+    })
+    qcloud.request({
+      url: config.service.movieList,
+      success: result => {
+        wx.hideLoading()
+
+        if (!result.data.code) {
+          this.setData({
+            productList: result.data.data
+          })
+        } else {
+          wx.showToast({
+            title: '电影数据加载失败',
+          })
+        }
+      },
+      fail: result => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '电影数据加载失败',
+        })
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
